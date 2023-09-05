530af4a21acbfb7a9558744b7db4fdafdbaaaabc
Daniel Vargas <devg28@gmail.com>
2023-09-04 22:11:12 -0400

    assets initial commit.

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class handles the movement of the player with given input from the input manager
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    public Shooter playerShooter;
+
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once before the first Update call
+    /// Input:
+    /// none
+    /// Return:
+    /// void (no return)
+    /// </summary>
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// Description:
+    /// Standard Unity function called once every frame
+    /// Input:
+    /// none
+    /// Return:
+    /// void (no return)
+    /// </summary>
+    void Update()
+    {
+
+    }
+}
